35df424002d3a857a72e9fb1163a57ee80e505ed
cima22 <gabri@DESKTOP-U95MR02.localdomain>
2022-02-02 20:50:11 +0100

    Rand generator

--- kdtree.c
+++ kdtree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <omp.h>
 
 #if !defined(DOUBLE_PRECISION)
@@ -9,6 +10,7 @@ typedef float float_t;
 typedef double float_t;
 #endif
 #define NDIM 2
+#define N 10
 
 //-------------------- Data Structures --------------------------------------------------------
 
@@ -22,28 +24,61 @@ typedef struct {
 
 //--------------------- Functions Declaration -------------------------------------------------
 
-knode * build_kdtree(kpoint * points, int N, int ndim, int axis);
+knode * build_kdtree(kpoint * points, int n, int ndim, int axis);
 
 int choose_splitting_dimension(int axis, int ndim);
 
-kpoint* choose_splitting_point(kpoint * points, int N, int ndim, int axis);
+kpoint* choose_splitting_point(kpoint * points, int n, int ndim, int axis);
+
+kpoint * initialize();
 
 //---------------------- Main -----------------------------------------------------------------
 
 int main(int argc, char* argv[]){
 
- printf("Hemlo\n"); 
-
+ kpoint * points = initialize();
+ 
+ for(int i = 0; i < N; i++){
+ printf("%f - %f\n", points[i][0], points[i][1]);
+ }
+ 
  return 0;
 }
 
 //----------------------- Functions -----------------------------------------------------------
 
+kpoint * initialize(){
+ 
+ kpoint * points = (kpoint *) malloc(N * sizeof(kpoint));
+
+ if(points == NULL){
+  printf("Problem with malloc()");
+  return NULL;
+ }
+
+ srand48((int) getpid());
+
+ for(int i = 0; i < N; i++){
+  points[i][0] = drand48();
+  points[i][1] = drand48();
+ }
+
+ return points;
+}
+
+knode * build_kdtree(kpoint * points, int n, int ndim, int axis){
+ 
+}
+
 int choose_splitting_dimension(int axis, int ndim){ return (axis + 1) % ndim; }
 
-kpoint* choose_splitting_point(kpoint* points, int N, int ndim, int axis){
+kpoint* choose_splitting_point(kpoint* points, int n, int ndim, int axis){
  
- 
+ //sort points
+ //
+ //choose median [n/2] or [floor(n/2)]
+ //
+ //return median
 
  return NULL;
 }
