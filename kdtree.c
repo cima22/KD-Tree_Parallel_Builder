9bee66dd735a3644bbdec27d990358abd6101327
cima22 <gabri@DESKTOP-U95MR02.localdomain>
2022-02-02 16:58:35 +0100

    ciao

--- kdtree.c
+++ kdtree.c
@@ -10,6 +10,8 @@ typedef double float_t;
 #endif
 #define NDIM 2
 
+//-------------------- Data Structures --------------------------------------------------------
+
 typedef float_t kpoint[NDIM];
 
 typedef struct {
@@ -18,15 +20,27 @@ typedef struct {
 	struct knode *left, *right;
 } knode;
 
+//--------------------- Functions Declaration -------------------------------------------------
+
 knode * build_kdtree(kpoint * points, int N, int ndim, int axis);
 
-int choose_splitting_dimension(int axis);
+int choose_splitting_dimension(int axis, int ndim);
 
 kpoint* choose_splitting_point(kpoint * points, int N, int ndim, int axis);
 
+//---------------------- Main -----------------------------------------------------------------
+
 int main(int argc, char* argv[]){
 
- 
+ printf("Hemlo\n"); 
 
  return 0;
+}
+
+//----------------------- Functions -----------------------------------------------------------
+
+int choose_splitting_dimension(int axis, int ndim){ return (axis + 1) % ndim; }
+
+kpoint* choose_splitting_point(kpoint* points, int N, int ndim, int axis){
+ return NULL;
 }
