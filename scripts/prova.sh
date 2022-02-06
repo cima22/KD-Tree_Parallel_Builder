1c6fa3e2a8137f5bdd80e19cc47626fea30621b6
Gabriele Cimador <cima@login.hpc>
2022-02-06 20:31:38 +0100

    brividi

--- scripts/prova.sh
+++ scripts/prova.sh
@@ -10,8 +10,8 @@ export OMP_NESTED=TRUE
 
 cd $PBS_O_HOME/hpc/Assignment2
 echo "nested,timing" > times/omp.csv
-gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
 cd src
+gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
 ./kdtree_omp 100000000 >> ../times/omp.csv
 export OMP_NESTED=FALSE
 ./kdtree_omp 100000000 >> ../times/omp.csv
