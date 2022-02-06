80509a31c51fb5a04b38451455c6cfe3c51f6292
cima22 <gab.cima22@gmail.com>
2022-02-06 13:50:53 +0100

    script

--- scripts/prova.sh
+++ scripts/prova.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 #PBS -q dssc
-#PBS -l nodes=2:ppn=24
+#PBS -l nodes=1:ppn=24
 #PBS -l walltime=01:40:00
-#PBS -N ringostarr
+#PBS -N tReE
 
-module load openmpi-4.1.1+gnu-9.3.0
-cd $PBS_O_HOME/hpc/Assigment1/section_1_measurements
-echo "processors,timing" > ringdo3.csv
-for i in {1..48..1}
-do
-  mpirun -np $i ./ring 2>/dev/null >> ringdo3.csv
-done
+export OMP_NUM_THREADS=128
+export OMP_NESTED=TRUE
+
+cd $PBS_O_HOME/hpc/Assignment2
+echo "nested,timing" > times/omp.csv
+gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
+cd src
+./kdtree_omp 100000000 >> ../times/omp.csv
+export OMP_NESTED=FALSE
+./kdtree_omp 100000000 >> ../times/omp.csv
+
