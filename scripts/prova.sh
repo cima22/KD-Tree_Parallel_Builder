7107c66264deca2c39a934300dd21005e285ef15
Gabriele Cimador <cima@login.hpc>
2022-02-07 16:12:20 +0100

    tests

--- scripts/prova.sh
+++ scripts/prova.sh
@@ -6,18 +6,18 @@
 #PBS -N tReE
 
 export OMP_NESTED=TRUE
-N=1000000000
+N=100000000
 
 cd $PBS_O_HOME/hpc/Assignment2
 echo "timing,dim,threads" > times/omp.csv
 cd src
 gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
 
+export OMP_NUM_THREADS=16
+./kdtree_omp $N >> ../times/omp.csv
 export OMP_NUM_THREADS=32
 ./kdtree_omp $N >> ../times/omp.csv
 export OMP_NUM_THREADS=64
 ./kdtree_omp $N >> ../times/omp.csv
 export OMP_NUM_THREADS=128
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=256
 ./kdtree_omp $N >> ../times/omp.csv
