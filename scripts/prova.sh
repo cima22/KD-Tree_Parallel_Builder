1809b7014ffd31b3717a647eb0ac16aa93eeb209
Gabriele Cimador <cima@login.hpc>
2022-02-08 16:11:02 +0100

    scripts

--- scripts/prova.sh
+++ scripts/prova.sh
@@ -12,19 +12,8 @@ echo "timing,dim,threads" > times/omp.csv
 cd src
 gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
 
-export OMP_NUM_THREADS=1
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=2
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=4
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=8
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=16
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=24
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=48
-./kdtree_omp $N >> ../times/omp.csv
-export OMP_NUM_THREADS=96
-./kdtree_omp $N >> ../times/omp.csv
+for t in 1 2 4 8 16 24 32 64 128 256
+do
+ export OMP_NUM_THREADS=$t
+ ./kdtree_omp $N >> ../times/omp.csv
+done
--- scripts/prova_gpu.sh
+++ scripts/prova_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -q dssc_dssc_gpu
+#PBS -q dssc_gpu
 #PBS -l nodes=1:ppn=48
 #PBS -l walltime=02:00:00
 #PBS -N tReE_gPu
@@ -12,21 +12,8 @@ echo "timing,dim,threads" > times/omp_gpu.csv
 cd src
 gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
 
-export OMP_NUM_THREADS=1
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=2
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=4
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=8
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=16
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=24
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=48
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=96
-./kdtree_omp $N >> ../times/omp_gpu.csv
-export OMP_NUM_THREADS=128
-./kdtree_omp $N >> ../times/omp_gpu.csv
+for t in 1 2 4 8 16 32 48 64 128 256
+do
+ export OMP_NUM_THREADS=$t
+ ./kdtree_omp $N >> ../times/omp_gpu.csv
+done
