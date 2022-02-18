88df9f9272788bae17c309045e1f8f4d27f4d49a
cima22 <gabri@DESKTOP-U95MR02.localdomain>
2022-02-18 12:20:25 +0100

    script

--- scripts/prova.sh
+++ scripts/prova.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 #PBS -q dssc
-#PBS -l nodes=1:ppn=24
+#PBS -l nodes=2:ppn=24
 #PBS -l walltime=02:00:00
 #PBS -N tReE
+
+module load openmpi-4.1.1+gnu-9.3.0
 
 N=100000000
 
 cd $PBS_O_HOME/hpc/Assignment2
 echo "timing,dim,threads" > times/omp.csv
+echo "timing,dim,procs" > times/mpi.csv
 cd src
-gcc -fopenmp -std=gnu99 -o kdtree_omp kdtree_omp.c
+gcc -fopenmp -std=gnu99 -o ../bin/kdtree_omp kdtree_omp.c
+mpicc -std=gnu99 -o ../bin/kdtree_mpi kdtree_mpi.c -lm
 
+cd ../bin
 for t in 1 2 4 8 16 24 32 64 128 256
 do
  export OMP_NUM_THREADS=$t
  ./kdtree_omp $N >> ../times/omp.csv
+done
+for p in 1 2 4 8 16 32
+do
+ mpirun -np $p ./kdtree_mpi $N >> ../times/mpi.csv
 done
